1e8cee84adfd1189e95fe35891cee9c53af2c8b4
ohad123012 <ohadlazar12@gmail.com>
2024-11-04 14:11:17 +0200

    refactor: changed app ts

--- apps/client/src/app/app.tsx
+++ apps/client/src/app/app.tsx
@@ -1,14 +1,9 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import styles from './app.module.css';
+import { FC } from 'react';
 
-import NxWelcome from './nx-welcome';
-
-export function App() {
-  return (
-    <div>
-      <NxWelcome title="client" />
-    </div>
-  );
-}
+export const App: FC = () => {
+  return <div>HEY</div>;
+};
 
 export default App;
